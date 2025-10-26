ef3b13da08e278ad009db312d9495f5a4c57b267
fajar <nugrahafajar37@gmail.com>
2025-10-26 16:49:30 +0700

    update: submodule

--- mockpass/lib/express/myinfo/index.js
+++
@@ -1,9 +0,0 @@
-const { config: consent } = require('./consent')
-const controllers = require('./controllers')
-
-const { pki } = require('../../crypto/myinfo-signature')
-
-module.exports = {
-  consent,
-  v3: controllers('v3', pki),
-}
