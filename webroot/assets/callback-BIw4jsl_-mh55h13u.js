bf89420c4b59b4f19de87b3e433f7d91f0d6733c
fajar <nugrahafajar37@gmail.com>
2025-11-20 00:26:20 +0700

    Update asset references in HTML files for logout and index pages
    - Changed favicon and stylesheet links in fc-logout.html, index.html, and other related files to new versions.
    - Updated script and module preload links to reflect new asset versions in index.html and mockpass/index.html.

--- webroot/assets/callback-BIw4jsl_-mh55h13u.js
+++
@@ -1,2 +0,0 @@
-import{o as e}from"./oidc-BozLfHUj-mh55h13u.js";new URLSearchParams(window.location.search)?.get("type")?.toLocaleLowerCase()==="silent"?e.signinSilentCallback().then(()=>{window.location.replace("/aceas/")}):e.signinCallback().then(()=>{window.location.replace("/aceas/")});
-//# sourceMappingURL=callback-BIw4jsl_-mh55h13u.js.map
---
+++ webroot/assets/callback-Dv0YUXwk-mi69ve73.js
@@ -0,0 +1,2 @@
+import{o as e}from"./oidc-D1XT6kQq-mi69ve73.js";new URLSearchParams(window.location.search)?.get("type")?.toLocaleLowerCase()==="silent"?e.signinSilentCallback().then(()=>{window.location.replace("/aceas/")}):e.signinCallback().then(()=>{window.location.replace("/aceas/")});
+//# sourceMappingURL=callback-Dv0YUXwk-mi69ve73.js.map
